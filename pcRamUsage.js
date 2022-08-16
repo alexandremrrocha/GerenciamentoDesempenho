2455327d02f18eba06ea688a73e1614d8c229ce3
Alexandre Miranda Rezende Rocha <alexandremrr2014@gmail.com>
2022-08-16 19:54:57 -0300

    FEAT: Adicionando o monitoramento do uso da CPU em porcentagem

--- pcRamUsage.js
+++ pcRamUsage.js
@@ -1,21 +1,29 @@
 const os = require('os');
+const osu = require('node-os-utils');
+const cpu = osu.cpu
 
-setInterval(() =>{
-    const {arch, platform, freemem, totalmem} = os;
-    const TotalRAM  = (((totalmem()/1024)/1024)/1024).toFixed(3);
-    const FreeRAM   = (((freemem()/1024)/1024)/1024).toFixed(3);
-    const Usage     = (((FreeRAM/TotalRAM)*100)-100)*(-1);
-    const stats = {
-        OS: platform(),
-        Arch: arch(),
-        TotalRAM:   `${TotalRAM} GB`,
-        FreeRAM:    `${FreeRAM} GB`,
-        UsageRAM:   `${parseInt(Usage)}%`,
-    };
-    console.clear();
-    console.table(stats);
-    
-    exports.stats = stats;
-    
-}, 1000);
+async function CPU() {
+  const VarCPUUsage = await cpu.usage()
+  console.log(`Uso da CPU: ${VarCPUUsage}%`)  
+}
+
+setInterval(() => {
+  const { arch, platform, freemem, totalmem } = os;
+  const TotalRAM = (((totalmem() / 1024) / 1024) / 1024).toFixed(3);
+  const FreeRAM = (((freemem() / 1024) / 1024) / 1024).toFixed(3);
+  const RAMUsage = (((FreeRAM / TotalRAM) * 100) - 100) * (-1);
+  const Model = cpu.model()
+  
+  console.clear();
+  
+  console.log(`Modelo da CPU: ${Model}`)
+  console.log(`Sistema Operacional: ` + platform())
+  console.log(`Arquitetura: ` + arch())
+  console.log(`Total de RAM: ${TotalRAM} GB`)
+  console.log(`RAM Livre: ${FreeRAM} GB`)
+  console.log(`Uso da RAM: ${parseInt(RAMUsage)}%`)
+  CPU()
+
+}, 5000);
+
 
