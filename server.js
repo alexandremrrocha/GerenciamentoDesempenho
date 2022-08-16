48346076e9ebdb1add058fd9a640c0b4c9d8de47
Alexandre Miranda Rezende Rocha <alexandremrr2014@gmail.com>
2022-08-16 19:54:14 -0300

    STYLE: Mudando a mensagem de resposta ao acessar a url do servidor

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const http = require('http');
-const host  = 'http://localhost';
-const port  =  3000;
+const host = 'http://localhost';
+const port = 3000;
 const stats = require('./pcRamUsage.js');
-var url
+let url
 
-http.createServer((req, res)  =>{
+
+http.createServer((req, res) => {
     url = req.url;
+    res.end('<h2>Servidor esta rodando!</h2> <h2>Cheque o terminal!</h2>')
 
-    if (url === '/stats'){
-        res.end(JSON.stringify(stats, null, 2));
-    } else{
-        res.end('<h1>Seja bem vindo</h1>')
-    }
 
 }).listen(port, () => console.log(`Server is running in ${host}:${port}`));
